839e41041b433e5a18ff23d8be7e4e532a148e23
5kym4rk <victorymaserati@gmail.com>
2025-10-23 15:39:54 +0700

    series mỗi ngày một bài code: 4 days

---
+++ SUM/SUM07.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_N 1000000
+
+double S[MAX_N + 1];
+
+// Tính trước biểu thức
+void precompute() {
+    S[0] = 0.0;
+    for (int i = 1; i <= MAX_N; i++) {
+        S[i] = sqrt(i + S[i - 1]);
+    }
+}
+
+int main() {
+    precompute();
+
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        int n;
+        scanf("%d", &n);
+        printf("%.5lf\n", S[n]);
+    }
+}
